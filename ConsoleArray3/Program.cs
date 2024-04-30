45979aa2aebb2373a4c17a089824145751a91692
Usuario <Usuario@CPD02-M07>
2024-04-30 11:19:37 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleArray3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleArray3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nome = new String[5];
+
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("Digite o nome do aluno: ");
+                nome[i] = Console.ReadLine();
+            }
+            for (int j = 0; j <= 4; j++)
+            {
+                Console.WriteLine("Aluno: " + nome[j]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
